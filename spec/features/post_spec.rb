fff0c756ed6477f7a8bc4a5e82f5ca38187e899d
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-08-14 19:19:52 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,5 +2,9 @@ require 'rails_helper'
 
 describe 'new post' do
   it 'ensures that the form route works with the /new action' do
+    visit new_post_ath
+    
+  expect(page.status_code).to eq(200)
+end
 
 end
